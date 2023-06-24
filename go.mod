56f95dd70de4899611b8ddf1884188b068902bc7
gitpushy <alex@deployport.com>
2023-06-24 15:52:54 +0000

    Database client

--- go.mod
+++ go.mod
@@ -3,13 +3,21 @@ module github.com/deployport/pavement
 go 1.20
 
 require (
+	entgo.io/ent v0.12.3
 	github.com/golang-migrate/migrate/v4 v4.16.2
+	github.com/jackc/pgx-zap v0.0.0-20221202020421-94b1cb2f889f
+	github.com/jackc/pgx/v5 v5.3.1
 	go.uber.org/zap v1.24.0
 )
 
 require (
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 )
--- sql/client.go
+++ sql/client.go
@@ -1,28 +1,95 @@
 package sql
 
-type Client struct {
+import (
+	"context"
+	"database/sql"
+	"fmt"
+
+	"entgo.io/ent/dialect"
+	entsql "entgo.io/ent/dialect/sql"
+	sqlconfig "github.com/deployport/pavement/sql/config"
+	sqlmigrations "github.com/deployport/pavement/sql/migrations"
+	pgxzap "github.com/jackc/pgx-zap"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/stdlib"
+	_ "github.com/jackc/pgx/v5/stdlib" // register pgx driver
+	"github.com/jackc/pgx/v5/tracelog"
+	"go.uber.org/zap"
+)
+
+// NewClient opens a connection to the database.
+// This instance can be shared safely across requests
+// as connection pooling is handled by the driver.
+func NewClient[TTx EntTransaction, TClient EntClient[TTx]](
+	ctx context.Context,
+	logger *zap.Logger,
+	config sqlconfig.Connection,
+	catalog *sqlmigrations.Catalog,
+	entCreator func(driver *entsql.Driver) TClient,
+) (*Client[TTx, TClient], error) {
+	logger = logger.Named("maindb")
+	databaseURL := config.URL
+	connConfig, err := pgx.ParseConfig(databaseURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse database url, %w", err)
+	}
+	dbLogger := pgxzap.NewLogger(logger)
+	connConfig.Tracer = &tracelog.TraceLog{
+		Logger:   dbLogger,
+		LogLevel: tracelog.LogLevelInfo,
+	}
+	connStr := stdlib.RegisterConnConfig(connConfig)
+	db, err := sql.Open("pgx", connStr)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database, %w", err)
+	}
+	if err := db.PingContext(ctx); err != nil {
+		return nil, fmt.Errorf("failed to connect database, %w", err)
+	}
+	drv := entsql.OpenDB(dialect.Postgres, db)
+	return &Client[TTx, TClient]{
+		Client:  entCreator(drv),
+		Catalog: catalog,
+	}, nil
+}
+
+// EntTransaction is an interface for ent.Tx
+type EntTransaction interface {
+	Rollback() error
+	Commit() error
+}
+
+// EntClient is an interface for ent.Client
+type EntClient[TTx EntTransaction] interface {
+	Tx(ctx context.Context) (TTx, error)
+}
+
+// Client entdata is a connected client instance
+type Client[TTx EntTransaction, TClient EntClient[TTx]] struct {
+	Client  TClient
+	Catalog *sqlmigrations.Catalog
 }
 
 // WithTx executes a function in the context of a transaction
-// func (client *Client) WithTx(ctx context.Context, fn func(tx *ent.Tx) error) error {
-// 	tx, err := client.Tx(ctx)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer func() {
-// 		if v := recover(); v != nil {
-// 			tx.Rollback()
-// 			panic(v)
-// 		}
-// 	}()
-// 	if err := fn(tx); err != nil {
-// 		if rerr := tx.Rollback(); rerr != nil {
-// 			err = fmt.Errorf("rolling back transaction: %w", rerr)
-// 		}
-// 		return err
-// 	}
-// 	if err := tx.Commit(); err != nil {
-// 		return fmt.Errorf("committing transaction: %w", err)
-// 	}
-// 	return nil
-// }
+func (client *Client[TTx, TClient]) WithTx(ctx context.Context, fn func(tx TTx) error) error {
+	tx, err := client.Client.Tx(ctx)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if v := recover(); v != nil {
+			tx.Rollback()
+			panic(v)
+		}
+	}()
+	if err := fn(tx); err != nil {
+		if rerr := tx.Rollback(); rerr != nil {
+			err = fmt.Errorf("rolling back transaction: %w", rerr)
+		}
+		return err
+	}
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("committing transaction: %w", err)
+	}
+	return nil
+}
